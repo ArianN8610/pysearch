428f3834a37dc3d0af270716b82345b49d320000
Arian <ariannasiri86@gmail.com>
2025-03-17 14:54:02 +0330

    Search for specified file extensions

--- pysearch/cli.py
+++ pysearch/cli.py
@@ -15,7 +15,9 @@ from .searcher import search_in_names, search_in_file_contents
 @click.option('-d', '--directories', is_flag=True, help='Search only in directory names')
 @click.option('-c', '--content', is_flag=True, help='Search only in file contents')
 @click.option('-cs', '--case-sensitive', is_flag=True, help='Case sensitive in searches')
-def search(query: str, path: str, files: bool, directories: bool, content: bool, case_sensitive: bool):
+@click.option('--ext', type=click.STRING, default='',
+              help='Only display files with the specified extension (e.g. "py,js")')
+def search(query: str, path: str, files: bool, directories: bool, content: bool, case_sensitive: bool, ext: str):
     """Get query, search and display results"""
 
     if True not in (files, directories, content):
@@ -23,19 +25,19 @@ def search(query: str, path: str, files: bool, directories: bool, content: bool,
 
     # Search in file names and directory names and files content separately
     if files:
-        filename_results = search_in_names(path, query, case_sensitive)
+        filename_results = search_in_names(path, query, case_sensitive, ext)
         display_results(filename_results, 'Files', 'file')
     else:
         filename_results = []
 
-    if directories:
-        dir_results = search_in_names(path, query, case_sensitive, False)
+    if directories and not ext.strip():
+        dir_results = search_in_names(path, query, case_sensitive, is_file=False)
         display_results(dir_results, 'Directories', 'directory')
     else:
         dir_results = []
 
     if content:
-        content_results = search_in_file_contents(path, query, case_sensitive)
+        content_results = search_in_file_contents(path, query, case_sensitive, ext)
         display_results(content_results, 'Contents', 'content')
     else:
         content_results = []
--- pysearch/searcher.py
+++ pysearch/searcher.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from .utils import highlight_matches
 
 
-def search_in_names(base_path: str, query: str, case_sensitive: bool, is_file: bool = True) -> list[str]:
+def search_in_names(base_path: str, query: str, case_sensitive: bool, ext: str = '', is_file: bool = True) -> list[str]:
     """Search for file names"""
     base_path = Path(base_path)
     matches = []
@@ -13,17 +13,23 @@ def search_in_names(base_path: str, query: str, case_sensitive: bool, is_file: b
     if not case_sensitive:
         query = query.lower()
 
+    # Remove extra spaces and processing extensions
+    ext_list = [e.strip() for e in ext.split(',') if e.strip()]
+
     for p in base_path.rglob('*'):
         p_name = p.name.lower() if not case_sensitive else p.name
 
-        if query in p_name and (p.is_file() if is_file else p.is_dir()):
+        if query in p_name and ((is_file and p.is_file()) or (not is_file and p.is_dir())):
+            if is_file and ext_list and p.suffix[1:] not in ext_list:
+                continue  # Prevent unnecessary continuation in case of type mismatch
+
             p_name = p_name.replace(query, click.style(query, fg='green'))  # Specify the found part
-            matches.append(str(p.parent) + '\\' + p_name)
+            matches.append(f'{p.parent}\\{p_name}')
 
     return matches
 
 
-def search_in_file_contents(base_path: str, query: str, case_sensitive: bool) -> list[str]:
+def search_in_file_contents(base_path: str, query: str, case_sensitive: bool, ext: str) -> list[str]:
     """Search the contents of files"""
     base_path = Path(base_path)
     matches = []
@@ -31,22 +37,26 @@ def search_in_file_contents(base_path: str, query: str, case_sensitive: bool) ->
     if not case_sensitive:
         query = query.lower()
 
+    ext_list = [e.strip() for e in ext.split(',') if e.strip()]
+
     for file_path in base_path.rglob('*'):
-        if file_path.is_file():
-            try:
-                for num, line in enumerate(file_path.read_text(encoding='utf-8', errors='ignore').splitlines(), 1):
-                    line_content = line.lower() if not case_sensitive else line
+        if not file_path.is_file() or (ext_list and file_path.suffix[1:] not in ext_list):
+            continue
 
-                    if query in line_content:
-                        highlighted_snippet = highlight_matches(line_content.strip(), query, case_sensitive)
-                        count_query = len(re.findall(query, line_content))  # Count query in each line
+        try:
+            for num, line in enumerate(file_path.read_text(encoding='utf-8', errors='ignore').splitlines(), 1):
+                line_content = line.lower() if not case_sensitive else line
 
-                        matches.append(
-                            click.style(file_path, fg='blue')
-                            + click.style(f' (Line {num}) (Repeated {count_query} time(s)): ', fg='magenta')
-                            + highlighted_snippet
-                        )
-            except Exception:
-                continue  # Continue if there is an error (e.g. binary file)
+                if query in line_content:
+                    highlighted_snippet = highlight_matches(line_content.strip(), query, case_sensitive)
+                    count_query = len(re.findall(query, line_content))  # Count query in each line
+
+                    matches.append(
+                        click.style(file_path, fg='blue')
+                        + click.style(f' (Line {num}) (Repeated {count_query} time(s)): ', fg='magenta')
+                        + highlighted_snippet
+                    )
+        except Exception:
+            continue  # Continue if there is an error (e.g. binary file)
 
     return matches
